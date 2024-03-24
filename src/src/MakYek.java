f49d9b8862180afb2bc53c2cba3389e7fd0a902e
Emma Gosses <emmagosses@student.kdg.be>
2024-03-24 14:28:04 +0100

    made distinct classes

---
+++ src/src/MakYek.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class MakYek {
+/* Here we add the win conditions etc, to monitor spelverloop */
+
+
+
+    // capture logic // elimineer player_one of playe_two
+
+    // Check for win condition
+//            if (isGameOver(player)) {
+//                System.out.println("Game over! Player " + player + " wins.");
+//                System.exit(0); // Exit the game
+//            }
+//        } else {
+//            System.out.println("Invalid move. Try again.");
+//        }
+//    }
+
+
+    private boolean isGameOver(char player) {
+        throw new UnsupportedOperationException("Unimplemented method 'isGameOver'");
+    }
+
+
+
+
+
+}
---
+++ src/src/NonValidMoveException.java
@@ -0,0 +1,2 @@
+public class NonValidMoveException extends Exception {
+}
